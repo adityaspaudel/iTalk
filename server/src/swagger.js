1630478fb0ad06eadb8cfbb0838c224a1db62542
aaditya paudel <adityaspaudel@gmail.com>
2025-11-19 09:59:03 +0545

    swaggerjs api setupu made successflly

---
+++ server/src/swagger.js
@@ -0,0 +1,23 @@
+// src/swagger.js
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "Chat App API",
+    description: "Auto-generated Swagger documentation",
+  },
+  host: "localhost:8000",
+  schemes: ["http"],
+};
+
+const outputFile = "./swagger-output.json";
+
+// 👇 All your routes go here (no need for comments inside route files)
+const endpointsFiles = [
+  "./src/routes/userRoute.js",
+  // "./src/routes/messageRoute.js",
+];
+
+swaggerAutogen(outputFile, endpointsFiles).then(() => {
+  console.log("📘 Swagger JSON generated automatically!");
+});
