684cf8079556c838dae051f52657ae615d237189
aaditya paudel <adityaspaudel@gmail.com>
2025-11-19 07:21:47 +0545

    dotenv setup made

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,27 @@
+const express = require("express");
+const app = express();
+const dbConnect = require("./db/connection");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config({ path: "" });
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
 
+// database connection
+dbConnect();
 
-console.log("hello");
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cookieParser());
+app.use(compression());
+
+// app
+const port = process.env.port;
+app.listen(port, () => {
+  console.log(`application is listening on port ${port}`);
+});
